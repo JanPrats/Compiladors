19753a31536b333320bd3b81ea1a0459f827ac95
Andrea <131747892+andreasallo@users.noreply.github.com>
2026-01-20 19:37:17 +0100

    fisrt propose of module errors .h and .c

--- src/module_errors/module_errors.h
+++ src/module_errors/module_errors.h
@@ -8,6 +8,30 @@
  * errors and warnings encountered during preprocessing.
  */
 
-void module_errors_run(void);
+#include <stdio.h>
+
+/* Error severity levels */
+typedef enum {
+    ERROR_WARNING,
+    ERROR_ERROR
+} ErrorLevel;
+
+/* Initializes the error system */
+void errors_init(void);
+
+/* Report an error or warning */
+void report_error(
+    ErrorLevel level,
+    const char *filename,
+    int line,
+    const char *message
+);
+
+/* Returns the number of errors detected */
+int errors_count(void);
+
+/* Called at the end of preprocessing */
+void errors_finalize(void);
 
 #endif
+
